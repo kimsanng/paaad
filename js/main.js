6bb22de5fd74737fbd87a51934dbcd3d0e6e14e5
kimsanng <kimgod3210@naver.com>
2023-04-05 16:45:22 +0900

    스크롤버튼

--- js/main.js
+++ js/main.js
@@ -283,7 +283,32 @@ mapEls.forEach(function(el){
 
 
 
+// lodash
 
+let toTopEl = document.querySelector('#to-top');
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 500){
+       
+        //버튼 보이기
+        gsap.to(toTopEl, .2,{
+            x: 0
+        });
+    } else{
+      
+        //버튼 숨기기
+        gsap.to(toTopEl, .2,{
+            x: 100
+        });
+    }
+}, 300)); //_.throttle(함수,시간)
+
+
+toTopEl.addEventListener('click', function(){
+    gsap.to(window, .7,{
+        scrollTo: 0
+    });
+})
 
 
 
